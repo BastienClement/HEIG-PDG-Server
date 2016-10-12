eb55224199f2ee55cef3ff557c9aac6ea16c510f
galedric <g@ledric.me>
2016-10-12 15:56:23 +0200

    Some POST requests do not have a body

--- app/filters/EnsureJson.scala
+++ app/filters/EnsureJson.scala
@@ -12,11 +12,6 @@ class EnsureJson @Inject()(
 		implicit override val mat: Materializer,
 		exec: ExecutionContext) extends Filter {
 
-	private def methodHasBody(method: String): Boolean = method match {
-		case "POST" | "PUT" | "PATCH" => true
-		case _ => false
-	}
-
 	private def wrapResponse(res: Future[Result]): Future[Result] = res.flatMap {
 		case html if html.body.contentType.exists(_.startsWith("text/html")) =>
 			html.body.consumeData.map { body =>
@@ -31,17 +26,8 @@ class EnsureJson @Inject()(
 		case other => other
 	}
 
-	def nonJsonBody(contentType: Option[String]): Future[Result] = {
-		Results.BadRequest(Json.obj(
-			"error" -> "INVALID_BODY_CONTENT_TYPE",
-			"message" -> "Only 'text/json' and 'application/json' can be processed.",
-			"content-type" -> (contentType.getOrElse("none"): String)
-		))
-	}
-
 	override def apply(f: (RequestHeader) => Future[Result])(rh: RequestHeader): Future[Result] = {
-		if (methodHasBody(rh.method) && !rh.contentType.exists(_.contains("json"))) nonJsonBody(rh.contentType)
-		else if (rh.getQueryString("html").nonEmpty || rh.headers.get("Accept").exists(_.contains("html"))) f(rh)
+		if (rh.getQueryString("html").nonEmpty || rh.headers.get("Accept").exists(_.contains("html"))) f(rh)
 		else wrapResponse(f(rh))
 	}
 }
