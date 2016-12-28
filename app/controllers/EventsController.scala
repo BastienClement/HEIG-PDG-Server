bf35960ff00c8d4cbdd2e06886c9bcfeaccf487a
Bastien Clément <g@ledric.me>
2016-12-28 15:13:49 +0100

    Implement Point of Interest endpoints

--- app/controllers/EventsController.scala
+++ app/controllers/EventsController.scala
@@ -2,10 +2,10 @@ package controllers
 
 import com.google.inject.{Inject, Provider, Singleton}
 import controllers.api.ApiActionBuilder
-import models.{Event, Events}
+import models._
 import play.api.Application
 import play.api.libs.json.{JsNumber, JsObject, Json}
-import play.api.mvc.Controller
+import play.api.mvc.{Controller, Result}
 import scala.concurrent.Future
 import scala.util.Try
 import services.EventService
@@ -16,16 +16,29 @@ class EventsController @Inject() (events: EventService)
                                  (val app: Provider[Application])
 		extends Controller with ApiActionBuilder {
 
+	/** Ensures that an event is editable by the current user before executing the given action. */
+	private def ensureEventEditable[T <: Result](event: Int)
+	                                            (action: => Future[T])
+	                                            (implicit pov: Users.PointOfView): Future[Result] = {
+		events.canEditEvent(event).flatMap {
+			case true => action
+			case false => Future.successful(Forbidden('EVENT_EDIT_FORBIDDEN))
+		}
+	}
+
+	/** Fetches the list of all events. */
 	def list = AuthApiAction.async { req =>
 		Events.run.map { events =>
 			Ok(events.map(Json.toJson[Event]))
 		}
 	}
 
+	/** Fetches information about a specific event. */
 	def get(id: Int) = AuthApiAction.async { req =>
 		Events.findById(id).head.map(event => Ok(Json.toJson(event))).orElse(NotFound('EVENT_NOT_FOUND))
 	}
 
+	/** Creates a new event. */
 	def create = AuthApiAction.async(parse.tolerantJson) { req =>
 		Try {
 			(req.body.as[JsObject] ++ Json.obj("id" -> 0, "owner" -> req.user.id)).as[Event]
@@ -40,12 +53,57 @@ class EventsController @Inject() (events: EventService)
 		}.get
 	}
 
+	/** Searches for nearby events. */
 	def nearby(lat: Double, lon: Double, radius: Double) = AuthApiAction.async {
 		events.nearby((lat, lon), radius).map { events =>
 			val combined = events.map { case (event, distance) =>
 				Json.toJson(event).as[JsObject] + ("distance" -> JsNumber(distance))
 			}
 			Ok(Json.toJson(combined))
+		}
+	}
+
+	/** Fetches the list of Point of Interest for a given event. */
+	def listPOI(event: Int) = AuthApiAction.async {
+		PointsOfInterest.findByEvent(event).run.map { poi =>
+			Ok(Json.toJson(poi.map(Json.toJson(_))))
+		}
+	}
+
+	/** Fetches a specific Point of Interest for a given event. */
+	def getPOI(event: Int, id: Int) = AuthApiAction.async {
+		events.getPOI(event, id).map {
+			case Some(poi) => Ok(poi)
+			case None => NotFound('EVENT_POI_NOT_FOUND)
+		}
+	}
+
+	/** Creates a new Point of Interest for a given event. */
+	def createPOI(event: Int) = AuthApiAction.async(parse.tolerantJson) { implicit req =>
+		ensureEventEditable(event) {
+			val poi = (req.body.as[JsObject] ++ Json.obj("event" -> event)).as[PointOfInterest]
+			events.registerPOI(poi).map(poi => Ok(poi))
+		}
+	}
+
+	/** Updates a specific Point of Interest for a given event. */
+	def updatePOI(event: Int, id: Int) = AuthApiAction.async(parse.tolerantJson) { implicit req =>
+		ensureEventEditable(event) {
+			val poi = (req.body.as[JsObject] ++ Json.obj("event" -> event, "id" -> id)).as[PointOfInterest]
+			events.updatePOI(poi).map {
+				case true => NoContent
+				case false => NotFound('EVENT_POI_NOT_FOUND)
+			}
+		}
+	}
+
+	/** Removes a Point of Interest from a given event. */
+	def removePOI(event: Int, poi: Int) = AuthApiAction.async { implicit req =>
+		ensureEventEditable(event) {
+			events.removePOI(event, poi).map {
+				case true => NoContent
+				case false => NotFound('EVENT_POI_NOT_FOUND)
+			}
 		}
 	}
 }
--- app/models/PointsOfInterest.scala
+++ app/models/PointsOfInterest.scala
@@ -1,15 +1,17 @@
 package models
 
-import utils.Coordinates
+import play.api.libs.json._
 import utils.SlickAPI._
+import utils.{Coordinates, UsingImplicits}
 
-case class PointOfInterest(id: Int, event: Int, title: String, desc: String, lat: Double, lon: Double) {
+case class PointOfInterest(id: Int, event: Int, title: String, desc: String, lat: Double, lon: Double)
+		extends UsingImplicits[PointsOfInterest] {
 	def location: Coordinates = (lat, lon)
 }
 
 //noinspection TypeAnnotation
 class PointsOfInterest(tag: Tag) extends Table[PointOfInterest](tag, "pois") {
-	def id = column[Int]("id", O.PrimaryKey)
+	def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
 	def event = column[Int]("event_id")
 	def title = column[String]("title")
 	def desc = column[String]("description")
@@ -20,5 +22,26 @@ class PointsOfInterest(tag: Tag) extends Table[PointOfInterest](tag, "pois") {
 }
 
 object PointsOfInterest extends TableQuery(new PointsOfInterest(_)) {
+	def findById(id: Int) = PointsOfInterest.filter(poi => poi.id === id)
+	def findByEvent(event: Int) = PointsOfInterest.filter(poi => poi.event === event)
+	def findByKey(event: Int, id: Int) = PointsOfInterest.filter(poi => poi.event === event && poi.id === id)
 
+	implicit val PointOfInterestFormat = new Format[PointOfInterest] {
+		def writes(poi: PointOfInterest): JsValue = Json.obj(
+			"id" -> poi.id,
+			"event" -> poi.event,
+			"title" -> poi.title,
+			"desc" -> poi.desc,
+			"location" -> poi.location
+		)
+		def reads(json: JsValue): JsResult[PointOfInterest] = for {
+			id <- (json \ "id").validateOpt[Int].map(_.getOrElse(0))
+			event <- (json \ "event").validate[Int]
+			title <- (json \ "title").validate[String]
+			desc <- (json \ "desc").validateOpt[String].map(_.getOrElse(""))
+			location <- (json \ "location").validate[JsArray]
+			lat <- location(0).validate[Double]
+			lon <- location(1).validate[Double]
+		} yield PointOfInterest(id, event, title, desc, lat, lon)
+	}
 }
