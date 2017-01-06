39bbac04b57d24cde37e64ea942df06f54f75af6
Bastien Clément <g@ledric.me>
2017-01-06 23:05:14 +0100

    Add Require on Patch DSL

--- app/utils/Patch.scala
+++ app/utils/Patch.scala
@@ -21,27 +21,48 @@ case class Patch[E, U, C[_]](query: Query[E, U, C], mappings: Vector[Mapping[E, 
 	private def withMapping(mapping: Mapping[E, U, C]): Self = copy(mappings = mappings :+ mapping)
 
 	/** Defines a generic mapping */
-	def Map[F, G, T](extractor: JsObject => Option[T], fields: E => F)(implicit shape: FShape[F, T, G]): Self = {
+	def Map[F, G, T](extractor: JsObject => Option[T], fields: E => F)
+	                (implicit shape: FShape[F, T, G]): Self = {
 		withMapping(new Mapping[E, U, C] {
-			def materialize(query: Query[E, U, C], document: JsObject): Option[Action] = {
-				extractor(document).map(value => query.map(fields).update(value))
+			def materialize(query: Query[E, U, C], row: U, patch: JsObject): Option[Action] = {
+				extractor(patch).map { value =>
+					query.map(fields).update(value)
+				}
 			}
 		})
 	}
 
 	/** Defines a simple field mapping */
-	def MapField[T: Reads, G](key: String, column: E => Rep[T])(implicit shape: FShape[Rep[T], T, G]): Self = {
+	def MapField[T: Reads, G](key: String, column: E => Rep[T])
+	                         (implicit shape: FShape[Rep[T], T, G]): Self = {
 		Map(doc => (doc \ key).asOpt[T], column)
+	}
+
+	/** Checks a precondition that must hold for the currently existing document */
+	def Require(cond: U => Boolean, error: String = "requirement failed"): Self = {
+		withMapping(new Mapping[E, U, C] {
+			def materialize(query: Query[E, U, C], row: U, patch: JsObject): Option[Action] = {
+				if (!cond(row)) throw new IllegalStateException(error)
+				None
+			}
+		})
 	}
 
 	/** Execute the operation, using the provided patch document */
 	def Execute(document: JsObject)(implicit ec: ExecutionContext): Future[U] = {
-		query.exists.result.flatMap {
-			case true =>
-				val actions = mappings.map(m => m.materialize(query, document)).collect { case Some(action) => action }
+		(for {
+			count <- query.length.result
+			head <- query.result.headOption
+		} yield (count, head)).flatMap {
+			case (1, Some(row)) =>
+				val actions = mappings.map(m => m.materialize(query, row, document)).collect {
+					case Some(action) => action
+				}
 				DBIO.sequence(actions) andThen query.result.head
-			case false =>
+			case (0, _) =>
 				DBIO.failed(new NoSuchElementException)
+			case (n, _) =>
+				DBIO.failed(new IllegalStateException(s"query matched $n rows"))
 		}.transactionally.run
 	}
 }
@@ -49,7 +70,7 @@ case class Patch[E, U, C[_]](query: Query[E, U, C], mappings: Vector[Mapping[E, 
 object Patch {
 	sealed trait Mapping[E, U, C[_]] {
 		type Action = DBIOAction[Int, NoStream, Write]
-		def materialize(query: Query[E, U, C], document: JsObject): Option[Action]
+		def materialize(query: Query[E, U, C], row: U, patch: JsObject): Option[Action]
 	}
 
 	def apply[E, U, C[_]](query: Query[E, U, C]): Patch[E, U, C] = new Patch(query)
