443a785a04f84dfd475451a1ed3b08722f3d3b7b
Bastien Clément <g@ledric.me>
2017-01-08 18:11:06 +0100

    Notifications controller comments

--- app/controllers/NotificationsController.scala
+++ app/controllers/NotificationsController.scala
@@ -6,10 +6,20 @@ import play.api.Application
 import play.api.mvc.Controller
 import services.NotificationsService
 
+/**
+  * Notifications controllers.
+  *
+  * Allow the client to fetch pending notifications.
+  * This is an easy alternative to using a true push notification system.
+  *
+  * @param ns  the notification service
+  * @param app an instance of the Play application
+  */
 @Singleton
 class NotificationsController @Inject() (ns: NotificationsService)
                                         (val app: Provider[Application])
 		extends Controller with ApiActionBuilder {
+	/** Fetches pending notification for the client */
 	def notifications = AuthApiAction.async { implicit req =>
 		ns.fetch(req.user.id, req.token).map(list => Ok(list)).orElse(Conflict)
 	}
